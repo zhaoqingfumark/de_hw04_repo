1393e9224cef7e68e832407dc7da37861c224f9e
Mark Fu <zhaoqingfumark@gmail.com>
2025-03-07 17:16:22 +0000

    updatebyhw04

--- taxi_hw_04/models/core/dm_monthly_zone_revenue.sql
+++ taxi_hw_04/models/core/dm_monthly_zone_revenue.sql
@@ -1,29 +1,14 @@
 {{ config(materialized='table') }}
 
 with trips_data as (
-    select * from {{ ref('fact_trips') }}
+    select * from {{ ref('fct_taxi_trips_quarterly_revenue') }}
 )
     select 
     -- Revenue grouping 
-    pickup_zone as revenue_zone,
-    {{ dbt.date_trunc("month", "pickup_datetime") }} as revenue_month, 
-
+    year_quarter,
     service_type, 
-
     -- Revenue calculation 
-    sum(fare_amount) as revenue_monthly_fare,
-    sum(extra) as revenue_monthly_extra,
-    sum(mta_tax) as revenue_monthly_mta_tax,
-    sum(tip_amount) as revenue_monthly_tip_amount,
-    sum(tolls_amount) as revenue_monthly_tolls_amount,
-    sum(ehail_fee) as revenue_monthly_ehail_fee,
-    sum(improvement_surcharge) as revenue_monthly_improvement_surcharge,
-    sum(total_amount) as revenue_monthly_total_amount,
-
-    -- Additional calculations
-    count(tripid) as total_monthly_trips,
-    avg(passenger_count) as avg_monthly_passenger_count,
-    avg(trip_distance) as avg_monthly_trip_distance
+    sum(total_amount) as revenue_yearly_quarters_total_amount
 
     from trips_data
-    group by 1,2,3
+    group by 1,2
